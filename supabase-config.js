b39b0dea45a2603513cab8a25352ec3e7954d1f3
ballesk1000-hue <ballesk1000@gmail.com>
2026-09-20 21:41:27 -0500

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
+
+const SUPABASE_ANON_KEY = "TU_CLAVE_PUBLICA_DE_SUPABASE";
+
+window.MercaCuiSupabaseConfig = {
+    url: SUPABASE_URL,
+    anonKey: SUPABASE_ANON_KEY
+};
